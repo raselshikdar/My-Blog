a4a753e1de71c4868219efa16748ab96afd5f3d5
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-10-04 15:00:57 +0600

    Update NoteList.vue

--- components/content/NoteList.vue
+++ components/content/NoteList.vue
@@ -1,17 +1,17 @@
 <script lang="ts" setup>
-const { data: categories } = await useProjects()
+const { data: noteCategories } = await useNotes()
 </script>
 
 <template>
   <div class="space-y-10">
-    <div v-for="(category, cKey) in categories.body" :key="cKey">
+    <div v-for="(category, cKey) in noteCategories.body" :key="cKey">
       <SubTitle>{{ category.name }}</SubTitle>
       <div class="grid md:grid-cols-2 gap-5">
-        <NoteCard v-for="(project, pKey) in category.projects" :key="pKey" :project="note" />
+        <NoteCard v-for="(note, nKey) in category.notes" :key="nKey" :note="note" />
       </div>
     </div>
   </div>
-  <NuxtLink class="font-mono no-underline opacity-70  mt-8" :to="$route.path.split('/').slice(0, -1).join('/') || '/'">
+  <NuxtLink class="font-mono no-underline opacity-70 mt-8" :to="$route.path.split('/').slice(0, -1).join('/') || '/'">
     cd ..
   </NuxtLink>
 </template>
