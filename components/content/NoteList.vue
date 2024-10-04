32b92594b217bdecb18d55d23196100b618c25c6
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-10-04 15:47:35 +0600

    Update NoteList.vue

--- components/content/NoteList.vue
+++ components/content/NoteList.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
-const { data: noteCategories } = await useNotes()
+import { useNotes } from '~/composables/content'
+import NoteCard from './NoteCard.vue'  // Import your NoteCard component
+
+// Fetching the categories containing notes
+const { data: categories } = await useNotes()
 </script>
 
 <template>
   <div class="space-y-10">
-    <div v-for="(category, cKey) in noteCategories.body" :key="cKey">
+    <div v-for="(category, cKey) in categories.body" :key="cKey">
       <SubTitle>{{ category.name }}</SubTitle>
       <div class="grid md:grid-cols-2 gap-5">
+        <!-- Iterating over notes in each category -->
         <NoteCard v-for="(note, nKey) in category.notes" :key="nKey" :note="note" />
       </div>
     </div>
