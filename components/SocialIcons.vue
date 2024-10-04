984018f7dcf2d19fc76665ca3d7d1769e3f5543b
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-10-04 20:31:10 +0600

    Update SocialIcons.vue

--- components/SocialIcons.vue
+++ components/SocialIcons.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
+    <!-- Twitter Link -->
     <NuxtLink target="_blank" href="https://twitter.com/raselshikdar_" title="Visit my Twitter" class="group">
       <i-line-md-twitter class="icon icon--off" alt="Twitter Logo" />
       <i-line-md-twitter-twotone class="icon icon--on" alt="Twitter Logo" />
     </NuxtLink>
+
+    <!-- GitHub Link -->
     <NuxtLink target="_blank" href="https://github.com/raselshikdar" title="Visit my GitHub" class="group">
       <i-line-md-github class="icon icon--off" alt="Github Logo" />
       <i-line-md-github-loop class="icon icon--on" alt="Github Logo" />
     </NuxtLink>
-    <NuxtLink target="_blank" href="https://mailto:raselshikdar597@gmail.com" title="Contact my Email" class="group">
-      <i-carbon-email class="icon icon--off" alt="Discord Logo" />
-      <i-carbon-email class="icon icon--on" alt="Discord Logo" />
+
+    <!-- New Telegram Link (Replaces Facebook) -->
+    <NuxtLink target="_blank" href="https://t.me/rasel597" title="Visit my Telegram" class="group">
+      <i-line-md-telegram class="icon icon--off" alt="Telegram Logo" />
+      <i-line-md-telegram class="icon icon--on" alt="Telegram Logo" />
+    </NuxtLink>
+
+    <!-- Email Link -->
+    <NuxtLink target="_blank" href="mailto:raselshikdar597@gmail.com" title="Contact my Email" class="group">
+      <i-carbon-email class="icon icon--off" alt="Email Logo" />
+      <i-carbon-email class="icon icon--on" alt="Email Logo" />
+    </NuxtLink>
+
+    <!-- Website Link -->
+    <NuxtLink target="_blank" href="https://rasel.us.kg" title="Visit my Website" class="group">
+      <i-line-md-external-link-rounded class="icon icon--off" alt="Website Logo" />
+      <i-line-md-external-link class="icon icon--on" alt="Website Logo" />
     </NuxtLink>
   </div>
 </template>
 
 <style scoped>
 a {
-  @apply hover: text-gray-700 dark:hover:text-gray-300 p-2;
+  @apply hover:text-gray-700 dark:hover:text-gray-300 p-2;
 }
 </style>
