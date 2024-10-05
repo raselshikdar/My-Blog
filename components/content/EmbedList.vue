db4fb88544c0fd1826b9b9be1b890789ab135772
Rasel Shikdar <imrasel597@gmail.com>
2024-10-05 17:44:46 +0600

    Create EmbedList.vue

---
+++ components/content/EmbedList.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import EmbedCard from '../EmbedCard.vue' // Ensure the path is correct
+import type { Embed } from '~/types'
+
+const { data: categories } = await useEmbeds() // Fetching embed data
+</script>
+
+<template>
+  <div class="space-y-10">
+    <div v-for="(category, cKey) in categories.body" :key="cKey">
+      <SubTitle>{{ category.name }}</SubTitle>
+      <div class="grid md:grid-cols-2 gap-5">
+        <EmbedCard
+          v-for="(embed, eKey) in category.embeds" 
+          :key="eKey" 
+          :embed="embed" 
+        />
+      </div>
+    </div>
+  </div>
+  <NuxtLink class="font-mono no-underline opacity-70 mt-8" :to="$route.path.split('/').slice(0, -1).join('/') || '/'">
+    cd ..
+  </NuxtLink>
+</template>
